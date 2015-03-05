5d55c1b68572de31c647ad8cf9adb2334869d7a8
Irene1612 <vicmatil.igf@gmail.com>
2015-03-05 11:21:08 +0100

    Añadidas clases DaoJpaFactory y GenericDaoJpa.

---
+++ src/main/java/models/daos/jpa/DaoJpaFactory.java
@@ -0,0 +1,36 @@
+package models.daos.jpa;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import models.daos.DaoFactory;
+import models.daos.TemaDao;
+import models.daos.VotoDao;
+
+import org.apache.logging.log4j.LogManager;
+
+
+public class DaoJpaFactory extends DaoFactory {
+    private static final String PERSISTENCE_UNIT = "tictactoe";
+
+    private static EntityManagerFactory entityManagerFactory = Persistence
+            .createEntityManagerFactory(PERSISTENCE_UNIT);
+
+    public DaoJpaFactory() {
+        LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
+    }
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
+    }
+
+	@Override
+	public TemaDao getUserDao() {
+		return null;
+	}
+
+	@Override
+	public VotoDao getCategoryDao() {
+		return null;
+	}
+}
