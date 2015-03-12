af1213562469627a99a50db36c9a0c65f789a2d8
Irene1612 <vicmatil.igf@gmail.com>
2015-03-12 12:26:46 +0100

    Initial. Iniciada parte de JSP. Add home.jsp. Add index.html. Add Dispatcher.java. Update pom.xml por error en la construccion del proyecto.

---
+++ src/main/java/views/beans/jsp/Dispatcher.java
@@ -0,0 +1,46 @@
+package views.beans.jsp;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+
+
+@WebServlet("/jsp/*")
+public class Dispatcher extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private static String PATH_ROOT_VIEW = "/viewsJsp/";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    	
+        String action = request.getPathInfo().substring(1);
+        LogManager.getLogger(Dispatcher.class).debug("accion: " + action);
+        String view;
+        switch (action) {  
+//        	case "persona":
+//        		view = "";
+//        		break;
+	        default:
+	            view = "home";
+        }
+
+        this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
+                .forward(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+}
